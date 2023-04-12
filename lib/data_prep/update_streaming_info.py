c147a820d5498cbde0a7a01a00bf2b94d7647a9f
TD1138 <tomdevine1138@gmail.com>
2023-04-12 18:54:43 +0100

    created a new python script purely o update the streaming info of each film

---
+++ lib/data_prep/update_streaming_info.py
@@ -0,0 +1,14 @@
+from tqdm import tqdm
+from enrichment_utils import get_ingested_films, get_new_films, update_streaming_info
+from sqlite_utils import get_from_table
+
+# films_to_ingest = get_new_films()
+# films_to_ingest = get_ingested_films(error_count=2)
+films_to_ingest = ['f_0aXA2'] # OVERRIDE TO DEBUG SPECIFIC FILMS
+ingestion_limit = len(films_to_ingest)
+# ingestion_limit = 1000
+print('In total, there are {} films left to update streaming for'.format(len(films_to_ingest)))
+for film_id in tqdm(films_to_ingest[:ingestion_limit]):
+    film_title = get_from_table('FILM_TITLE', film_id, 'FILM_TITLE')
+    print('\n', film_id, film_title)
+    update_streaming_info(film_id)
