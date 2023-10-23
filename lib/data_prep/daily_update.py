b5ef140554eb4c12473ff969a238ff4981d23441
TD1138 <tomdevine1138@gmail.com>
2023-10-23 23:44:18 +0100

    include update most popular records in dailyupdate

--- lib/data_prep/daily_update.py
+++ lib/data_prep/daily_update.py
@@ -2,7 +2,7 @@ from selenium_utils import download_letterboxd_zip
 from export_utils import unzip_letterboxd_downloads, set_latest_export, cleanup_exports_folder, refresh_core_tables
 from enrichment_utils import ingest_new_films, ingest_new_people
 from error_utils import correct_all_errors
-from update_utils import update_oldest_records
+from update_utils import update_oldest_records, update_most_popular_records
 from algo_utils import run_algo
 import sys
 
@@ -17,9 +17,10 @@ else:
     unzip_letterboxd_downloads()
     set_latest_export()
     refresh_core_tables()
-ingest_new_films()
+ingest_new_films(film_limit=1000)
 ingest_new_people()
 correct_all_errors()
 update_oldest_records()
-run_algo()
+update_most_popular_records()
+run_algo('linear_regression')
 cleanup_exports_folder()
