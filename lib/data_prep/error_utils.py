cb0a0e4d59d7d5e872778a352bf8dbb82316ee4f
TD1138 <tomdevine1138@gmail.com>
2024-02-21 11:51:40 +0000

    cleaned up some of the queries generating the errors added additional error checks:  - run time  - multiple collection names for same collection

--- lib/data_prep/error_utils.py
+++ lib/data_prep/error_utils.py
@@ -1,7 +1,7 @@
 from tqdm import tqdm
 from sqlite_utils import get_film_ids_from_select_statement
 from enrichment_utils import ingest_film
-from tmdb_utils import update_tmbd_metadata
+from tmdb_utils import update_tmbd_metadata, get_tmbd_metadata
 from letterboxd_utils import get_ext_ids_plus_content_type, get_metadata_from_letterboxd, update_letterboxd_stats
 from dotenv import load_dotenv
 
@@ -110,6 +110,7 @@ def correct_tmdb_metadata_errors(film_ids=None, refresh=False, dryrun=False, fil
         keyword_films_to_correct = get_film_ids_from_select_statement(keyword_select_statement)    
         cast_films_to_correct = get_film_ids_from_select_statement(cast_select_statement)
         crew_films_to_correct = get_film_ids_from_select_statement(crew_select_statement)
+        runtime_films_to_correct = get_film_ids_from_select_statement(runtime_select_statement)
         total_films_to_correct = list(set(    \
             financials_films_to_correct       \
             + tmdb_stats_films_to_correct     \
@@ -117,6 +118,7 @@ def correct_tmdb_metadata_errors(film_ids=None, refresh=False, dryrun=False, fil
             + keyword_films_to_correct        \
             + cast_films_to_correct           \
             + crew_films_to_correct           \
+            + runtime_films_to_correct        \
             ))
         films_to_correct = total_films_to_correct[:film_limit]
     print('In total, there are {} new films to correct tmdb metadata for - correcting {}'.format(len(total_films_to_correct), len(films_to_correct)))
@@ -137,18 +139,46 @@ def correct_tmdb_metadata_errors(film_ids=None, refresh=False, dryrun=False, fil
     successful_films = len(films_to_correct) - errors
     print('Corrected tmdb metadata for {} films ({:.2%})'.format(successful_films, successful_films/len(films_to_correct)))
 
+def correct_collection_name_mismatches(film_ids=None, refresh=False, dryrun=False, film_limit=1000):
+    if film_ids:
+        total_films_to_correct = film_ids
+        films_to_correct = total_films_to_correct[:film_limit]
+    else:
+        collection_issue_films_to_correct = get_film_ids_from_select_statement(collection_issue_select_statement)
+        total_films_to_correct = collection_issue_films_to_correct
+        films_to_correct = total_films_to_correct[:film_limit]
+    print('In total, there are {} films with collection name mismatches - correcting {}'.format(len(total_films_to_correct), len(films_to_correct)))
+    if dryrun:
+        print(films_to_correct[:10])
+        return
+    elif len(films_to_correct) == 0:
+        return
+    errors = 0
+    for film_id in tqdm(films_to_correct):
+        try:
+            if refresh:
+                ingest_film(film_id)
+            else:
+                get_tmbd_metadata(film_id)
+        except:
+            errors += 1
+    successful_films = len(films_to_correct) - errors
+    print('Corrected collection names for {} films ({:.2%})'.format(successful_films, successful_films/len(films_to_correct)))
+
 def correct_all_errors(film_ids=None, refresh=False, dryrun=False, film_limit=100):
     load_dotenv(override=True)
     correct_letterboxd_stats_errors(film_ids, refresh=refresh, dryrun=dryrun, film_limit=film_limit)
     correct_letterboxd_metadata_errors(film_ids, refresh=refresh, dryrun=dryrun, film_limit=film_limit)
     correct_ext_ids_plus_content_type_errors(film_ids, refresh=refresh, dryrun=dryrun, film_limit=film_limit)
     correct_tmdb_metadata_errors(film_ids, refresh=refresh, dryrun=dryrun, film_limit=film_limit*10)
+    correct_collection_name_mismatches(film_ids, refresh=refresh, dryrun=dryrun, film_limit=film_limit)
 
 title_select_statement = ("""
 
 SELECT
-	  a.FILM_ID
-	 ,COALESCE(julianday('now') - julianday(c.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
+
+	 a.FILM_ID
+    ,COALESCE(julianday('now') - julianday(c.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
 
 FROM ALL_FILMS a
 
@@ -162,17 +192,18 @@ WHERE b.FILM_TITLE IS NULL
 OR b.FILM_TITLE = ""
 OR c.FILM_URL_TITLE IS NULL
 OR c.FILM_URL_TITLE = ""
-AND DAYS_SINCE_LAST_UPDATE > 3
+AND DAYS_SINCE_LAST_UPDATE > 7
 
 """)
 
 year_select_statement = """
 
 SELECT
-	  a.FILM_ID
-	 ,b.FILM_YEAR
-	 ,b.FILM_DECADE
-	 ,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
+
+	 a.FILM_ID
+	,b.FILM_YEAR
+	,b.FILM_DECADE
+	,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
 
 FROM ALL_FILMS a
 
@@ -182,16 +213,17 @@ ON a.FILM_ID = b.FILM_ID
 WHERE b.FILM_ID IS NULL
 OR b.FILM_YEAR = ""
 OR b.FILM_YEAR IS NULL
-AND DAYS_SINCE_LAST_UPDATE > 3
+AND DAYS_SINCE_LAST_UPDATE > 7
 
 """
 
 genre_select_statement = """
 
 SELECT
-	  a.FILM_ID
-	 ,b.FILM_GENRE
-	 ,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
+
+	 a.FILM_ID
+	,b.FILM_GENRE
+	,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
 
 FROM ALL_FILMS a
 
@@ -201,7 +233,7 @@ ON a.FILM_ID = b.FILM_ID
 WHERE b.FILM_ID IS NULL
 OR b.FILM_GENRE = ""
 OR b.FILM_GENRE IS NULL
-AND DAYS_SINCE_LAST_UPDATE > 3
+AND DAYS_SINCE_LAST_UPDATE > 7
 
 """
 
@@ -209,8 +241,9 @@ imdb_id_select_statement = ("""
 
 SELECT
 
-	  a.FILM_ID
-	 ,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
+	 a.FILM_ID
+	,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE_IMDB
+    ,COALESCE(julianday('now') - julianday(c.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE_TMDB
 
 FROM ALL_FILMS a
 
@@ -221,7 +254,8 @@ LEFT JOIN TMDB_ID c
 ON a.FILM_ID = c.FILM_ID
 
 WHERE b.IMDB_ID IS NULL
-AND DAYS_SINCE_LAST_UPDATE > 3
+AND DAYS_SINCE_LAST_UPDATE_IMDB > 3
+AND DAYS_SINCE_LAST_UPDATE_TMDB > 3
 AND c.VALID = 1
 
 """)
@@ -248,8 +282,8 @@ content_type_select_statement = ("""
 
 SELECT
 
-	  a.FILM_ID
-	 ,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
+	 a.FILM_ID
+	,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
 
 FROM ALL_FILMS a
 
@@ -299,7 +333,7 @@ LEFT JOIN FILM_LETTERBOXD_STATS b
 ON a.FILM_ID = b.FILM_ID
 
 WHERE COALESCE(b.FILM_WATCH_COUNT, 0) = 0
---AND DAYS_SINCE_LAST_UPDATE > 7
+AND DAYS_SINCE_LAST_UPDATE > 7
 
 """)
 
@@ -352,7 +386,7 @@ SELECT
 	 a.FILM_ID
 	,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
 
-FROM ALL_RELEASED_FILMS a
+FROM ALL_FILMS a
 
 LEFT JOIN FILM_KEYWORDS b
 ON a.FILM_ID = b.FILM_ID
@@ -376,7 +410,7 @@ SELECT
 	,b.CAST_ORDER
 	,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
 
-FROM ALL_RELEASED_FILMS a
+FROM ALL_FILMS a
 
 LEFT JOIN FILM_CAST b
 ON a.FILM_ID = b.FILM_ID
@@ -399,7 +433,7 @@ SELECT
 	 a.FILM_ID
 	,COALESCE(julianday('now') - julianday(b.CREATED_AT), 99) AS DAYS_SINCE_LAST_UPDATE
 
-FROM ALL_RELEASED_FILMS a
+FROM ALL_FILMS a
 
 LEFT JOIN FILM_CREW b
 ON a.FILM_ID = b.FILM_ID
@@ -411,4 +445,39 @@ WHERE b.PERSON_ID IS NULL
 AND c.VALID = 1
 AND DAYS_SINCE_LAST_UPDATE > 7
 
+""")
+
+runtime_select_statement = ("""
+
+SELECT
+
+	a.FILM_ID
+
+FROM ALL_FILMS a
+
+LEFT JOIN FILM_RUNTIME b
+ON a.FILM_ID = b.FILM_ID
+
+WHERE COALESCE(b.FILM_RUNTIME, 0) = 0
+
+""")
+
+collection_issue_select_statement = ("""
+
+WITH BASE_TABLE AS ( 
+
+	SELECT COLLECTION_ID, COUNT(DISTINCT COLLECTION_NAME) AS ISSUE
+	FROM FILM_COLLECTIONS
+	GROUP BY COLLECTION_ID
+	HAVING ISSUE > 1
+	
+	)
+	
+SELECT
+
+	b.FILM_ID
+	
+FROM BASE_TABLE a
+LEFT JOIN FILM_COLLECTIONS b
+ON a.COLLECTION_ID = b.COLLECTION_ID
 """)
