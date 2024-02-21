179dd38afd5d1042b5693abaab579e51db3ca11d
TD1138 <tomdevine1138@gmail.com>
2024-02-21 11:55:17 +0000

    now there are two frequently used apps, added a static port and url to the apps

--- start_EveryFilmRankedApp.bat
+++ start_EveryFilmRankedApp.bat
@@ -1,4 +1,4 @@
 cd C:/Users/tomdevine/desktop/dev/PersonalProjects/letterboxd-app
 call letterboxd-env/scripts/activate
 cd lib/streamlit
-streamlit run LetterboxdApp.py --server.port 8501 --server.baseUrlPath "LetterboxdApp"
+streamlit run EveryFilmRankedApp.py --server.port 8502 --server.baseUrlPath "EveryFilmRankedApp"
