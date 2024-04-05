b56b5cc27c6ef9753a3e2eba5e0e288b558af7ef
TD1138 <tomdevine1138@gmail.com>
2024-04-05 17:20:25 +0100

    fix chrome driver auto-install

--- lib/data_prep/selenium_utils.py
+++ lib/data_prep/selenium_utils.py
@@ -1,6 +1,8 @@
 import os
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 # from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -11,19 +13,18 @@ load_dotenv(override=True)
 
 def download_letterboxd_zip(hide_actions=True):
     try:
-        # chromedriver_autoinstaller.install()
-        # chrome_options = Options()
+        service=Service(ChromeDriverManager().install())
         chrome_options = webdriver.ChromeOptions()
         if hide_actions: chrome_options.add_argument("--headless")
         download_dir =  os.getenv('PROJECT_PATH')+'/db/raw_exports/'
         download_dir = download_dir.replace('/', '\\')
         prefs = {"download.default_directory" : download_dir, "directory_upgrade": True}
         chrome_options.add_experimental_option("prefs", prefs)
-        driver = webdriver.Chrome(options=chrome_options)
+        driver = webdriver.Chrome(service=service, options=chrome_options)
         driver.get(os.getenv('LETTERBOXD_SETTINGS_PAGE'))
         driver.implicitly_wait(10)
-#         accept_cookies = driver.find_element(By.CLASS_NAME, 'fc-button-label')
-#         accept_cookies.click()
+        # accept_cookies = driver.find_element(By.CLASS_NAME, 'fc-button-label')
+        # accept_cookies.click()
         # username = driver.find_element(By.XPATH, '//*[@id="signin-username"]')
         username = driver.find_element(By.XPATH, '//*[@id="field-username"]')
         username.send_keys(os.getenv('LETTERBOXD_USER'))
@@ -38,31 +39,3 @@ def download_letterboxd_zip(hide_actions=True):
         print('Download Successful')
     except:
         print('Download Failed')
-
-def return_logged_in_page_source(page_url, hide_actions=True):
-    try:
-        chromedriver_autoinstaller.install()
-        # chrome_options = Options()
-        chrome_options = webdriver.ChromeOptions()
-        if hide_actions: chrome_options.add_argument("--headless")
-        download_dir =  os.getenv('PROJECT_PATH')+'/db/raw_exports/'
-        download_dir = download_dir.replace('/', '\\')
-        prefs = {"download.default_directory" : download_dir, "directory_upgrade": True}
-        chrome_options.add_experimental_option("prefs", prefs)
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get(os.getenv('LETTERBOXD_SETTINGS_PAGE'))
-        driver.implicitly_wait(10)
-#         accept_cookies = driver.find_element(By.CLASS_NAME, 'fc-button-label')
-#         accept_cookies.click()
-        username = driver.find_element(By.XPATH, '//*[@id="signin-username"]')
-        username.send_keys(os.getenv('LETTERBOXD_USER'))
-        password = driver.find_element(By.XPATH, '//*[@id="signin-password"]')
-        password.send_keys(os.getenv('LETTERBOXD_PASSWORD'))
-        signin_button = driver.find_element(By.XPATH, '//*[@id="signin-form"]/fieldset/div[4]/div/input')
-        signin_button.click()
-        time.sleep(5)
-        driver.get(page_url)
-        page_source = driver.page_source
-        return page_source
-    except:
-        return None
