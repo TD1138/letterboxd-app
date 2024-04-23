7ac932fc8172d44256ba0821df032387c366bf8e
TD1138 <tomdevine1138@gmail.com>
2024-04-23 12:22:35 +0100

    created a quick daily update that just refreshes the download and the core tables

---
+++ lib/data_prep/daily_update_short.py
@@ -0,0 +1,15 @@
+from selenium_utils import download_letterboxd_zip
+from export_utils import unzip_letterboxd_downloads, set_latest_export, cleanup_exports_folder, refresh_core_tables
+from enrichment_utils import ingest_new_films
+from algo_utils import run_algo
+
+import warnings
+warnings.filterwarnings("ignore")
+
+download_letterboxd_zip(hide_actions=True)
+unzip_letterboxd_downloads()
+set_latest_export()
+refresh_core_tables()
+ingest_new_films()
+run_algo('linear_regression')
+cleanup_exports_folder()
