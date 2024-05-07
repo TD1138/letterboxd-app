a397016881dc0825b9575207d3da598b5880e8f8
TD1138 <tomdevine1138@gmail.com>
2024-05-07 15:17:37 +0100

    add precompute method to shorter daily update

--- lib/data_prep/daily_update_short.py
+++ lib/data_prep/daily_update_short.py
@@ -2,6 +2,7 @@ from selenium_utils import download_letterboxd_zip
 from export_utils import unzip_letterboxd_downloads, set_latest_export, cleanup_exports_folder, refresh_core_tables
 from enrichment_utils import ingest_new_films
 from algo_utils import run_algo
+from precompute_tables import precompute_tables
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -12,4 +13,5 @@ set_latest_export()
 refresh_core_tables()
 ingest_new_films()
 run_algo('linear_regression')
+precompute_tables()
 cleanup_exports_folder()
