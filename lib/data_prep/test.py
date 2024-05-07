1bac384a4c042cbdb11389748d83f770449d2a8a
TD1138 <tomdevine1138@gmail.com>
2024-05-07 15:18:30 +0100

    update test.py

--- lib/data_prep/test.py
+++ lib/data_prep/test.py
@@ -1,6 +1,6 @@
 from tqdm import tqdm
 from export_utils import refresh_core_tables
-from enrichment_utils import get_all_films, update_all_letterboxd_info, ingest_film, ingest_new_people
+from enrichment_utils import ingest_new_films, get_all_films, update_all_letterboxd_info, ingest_film, ingest_new_people
 from sqlite_utils import get_from_table, get_film_ids_from_select_statement, select_statement_to_df
 from tmdb_utils import update_tmdb_stats, update_tmbd_metadata, update_person_metadata
 from error_utils import correct_tmdb_metadata_errors, correct_all_errors, correct_letterboxd_stats_errors
@@ -16,38 +16,65 @@ warnings.filterwarnings("ignore")
 
 select_statement = ("""
 
-WITH DAYS_SINCE_LAST_STREAMING_UPDATE AS (
+WITH BASE_TABLE AS (
 
-SELECT FILM_ID, ROUND(AVG(DAYS_SINCE_LAST_UPDATE), 0) AS DAYS_SINCE_LAST_UPDATE 
-FROM ( SELECT FILM_ID, ROUND(COALESCE(julianday('now') - julianday(CREATED_AT), 99), 0) AS DAYS_SINCE_LAST_UPDATE FROM FILM_STREAMING_SERVICES )
-GROUP BY FILM_ID
+	SELECT
+	
+		a.FILM_ID
+		,b.FILM_TITLE
+		,c.FILM_YEAR
+		,ROW_NUMBER() OVER (PARTITION BY c.FILM_YEAR ORDER BY a.FILM_REVENUE DESC) AS YEAR_NUMBER
+		,c.FILM_YEAR || char(10) || ROW_NUMBER() OVER (PARTITION BY c.FILM_YEAR ORDER BY a.FILM_REVENUE DESC) || '. $' || PRINTF("%,d", a.FILM_REVENUE) AS TEXT
+	
+	FROM FILM_FINANCIALS a
+	
+	LEFT JOIN FILM_TITLE b
+	ON a.FILM_ID = b.FILM_ID
+	
+	LEFT JOIN FILM_YEAR c
+	ON a.FILM_ID = c.FILM_ID
+	
+	WHERE c.FILM_YEAR >= 1960
+	AND c.FILM_YEAR <= 2024
+	
+	AND a.FILM_ID != "f_025Gu"
+	AND a.FILM_ID != "f_01au2"
+	AND a.FILM_ID != 'f_01RCI'
+	AND a.FILM_ID != 'f_01b3Q'
+	)
+	
+, TOP_TEN_TABLE AS (
 
-)
+	SELECT * FROM BASE_TABLE
+	
+	WHERE YEAR_NUMBER <= 20
+	
+	)
 
-SELECT a.FILM_ID
-FROM FILM_ALGO_SCORE a
-LEFT JOIN DAYS_SINCE_LAST_STREAMING_UPDATE b
-ON a.FILM_ID = b.FILM_ID
-ORDER BY COALESCE(a.ALGO_SCORE, 0.01) * COALESCE(b.DAYS_SINCE_LAST_UPDATE, 365) DESC
+SELECT FILM_ID, FILM_TITLE, TEXT FROM TOP_TEN_TABLE
+WHERE FILM_YEAR = 2022
+OR FILM_YEAR = 2021
+	
+ORDER BY FILM_YEAR ASC, YEAR_NUMBER ASC
 
 """)
 
 # people_to_ingest = list(select_statement_to_df(select_statement)['PERSON_ID'].values)
-# films_to_ingest = get_film_ids_from_select_statement(select_statement)
+films_to_ingest = get_film_ids_from_select_statement(select_statement)
 # # films_to_ingest = get_film_ids_from_select_statement("SELECT * FROM FILM_RELEASE_INFO WHERE FILM_STATUS = 'Ended' OR FILM_STATUS = 'Returning Series'")
 # # films_to_ingest = get_all_films()
-# # films_to_ingest = ['f_0hK9G'] # OVERRIDE TO DEBUG SPECIFIC FILMS
-# ingestion_limit = len(films_to_ingest)
+# films_to_ingest = ['f_0bCLK'] # OVERRIDE TO DEBUG SPECIFIC FILMS
+ingestion_limit = len(films_to_ingest)
 # # ingestion_limit = 10
-# print('In total, there are {} films left to update letterboxd info for:'.format(len(films_to_ingest)))
-# for film_id in tqdm(films_to_ingest[:ingestion_limit]):
-# 	print('\n', film_id)
-# 	try:
-# 		ingest_film(film_id)
+print('In total, there are {} films left to update letterboxd info for:'.format(len(films_to_ingest)))
+for film_id in tqdm(films_to_ingest[:ingestion_limit]):
+	print('\n', film_id)
+	try:
+		ingest_film(film_id)
 # 	    # update_tmbd_metadata(film_id)
 # 		# update_all_letterboxd_info(film_id)
-# 	except Exception as e:
-# 	    print('UPDATE FAILED - {}'.format(e))
+	except Exception as e:
+	    print('UPDATE FAILED - {}'.format(e))
 
 # update_oldest_records(film_limit=10, dryrun=False)
 # refresh_core_tables()
@@ -90,4 +117,6 @@ ORDER BY COALESCE(a.ALGO_SCORE, 0.01) * COALESCE(b.DAYS_SINCE_LAST_UPDATE, 365) 
 
 # get_letterboxd_top_250()
 
-download_letterboxd_zip(hide_actions=False)
+# download_letterboxd_zip(hide_actions=False)
+
+# ingest_new_films(999)
