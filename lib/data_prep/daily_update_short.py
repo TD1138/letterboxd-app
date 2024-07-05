4b068d7eb04c21cbaa336364e474f90482a20b17
TD1138 <tomdevine1138@gmail.com>
2024-07-05 13:38:06 +0100

    add external db hosting to short daily update

--- lib/data_prep/daily_update_short.py
+++ lib/data_prep/daily_update_short.py
@@ -3,10 +3,12 @@ from export_utils import unzip_letterboxd_downloads, set_latest_export, cleanup_
 from enrichment_utils import ingest_new_films
 from algo_utils import run_algo
 from precompute_tables import precompute_tables
+from gcp_utils import download_db, upload_db
 
 import warnings
 warnings.filterwarnings("ignore")
 
+download_db()
 download_letterboxd_zip(hide_actions=True)
 unzip_letterboxd_downloads()
 set_latest_export()
@@ -15,3 +17,4 @@ ingest_new_films()
 run_algo('linear_regression')
 precompute_tables()
 cleanup_exports_folder()
+upload_db()
