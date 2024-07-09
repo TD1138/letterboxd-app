a0ea6e851deb973692065937025ad97d276bca1d
TD1138 <tomdevine1138@gmail.com>
2024-07-09 12:35:58 +0100

    update test.py

--- lib/data_prep/test.py
+++ lib/data_prep/test.py
@@ -1,10 +1,10 @@
 from tqdm import tqdm
 from export_utils import refresh_core_tables
-from enrichment_utils import ingest_new_films, get_all_films, update_most_popular_films, ingest_film, ingest_new_people
+from enrichment_utils import ingest_new_films, get_all_films, ingest_film, ingest_new_people
 from sqlite_utils import get_from_table, get_film_ids_from_select_statement, select_statement_to_df
 from tmdb_utils import update_tmdb_stats, update_tmbd_metadata, update_person_metadata
 from error_utils import correct_tmdb_metadata_errors, correct_all_errors, correct_letterboxd_stats_errors
-from update_utils import update_oldest_records, update_streaming_records, update_tmdb_metadata_records, update_recent_films, update_upcoming_films, update_most_popular_films, update_letterboxd_stats, update_letterboxd_top_250
+from update_utils import update_oldest_records, update_streaming_records, update_tmdb_metadata_records, update_recent_films, update_upcoming_films, update_letterboxd_stats, update_letterboxd_top_250
 from algo_utils import run_algo
 from selenium_utils import download_letterboxd_zip
 from justwatch_utils import update_streaming_info
@@ -60,21 +60,21 @@ ORDER BY FILM_YEAR ASC, YEAR_NUMBER ASC
 """)
 
 # people_to_ingest = list(select_statement_to_df(select_statement)['PERSON_ID'].values)
-films_to_ingest = get_film_ids_from_select_statement(select_statement)
+# films_to_ingest = get_film_ids_from_select_statement(select_statement)
 # # films_to_ingest = get_film_ids_from_select_statement("SELECT * FROM FILM_RELEASE_INFO WHERE FILM_STATUS = 'Ended' OR FILM_STATUS = 'Returning Series'")
 # # films_to_ingest = get_all_films()
 # films_to_ingest = ['f_0bCLK'] # OVERRIDE TO DEBUG SPECIFIC FILMS
-ingestion_limit = len(films_to_ingest)
+# ingestion_limit = len(films_to_ingest)
 # # ingestion_limit = 10
-print('In total, there are {} films left to update letterboxd info for:'.format(len(films_to_ingest)))
-for film_id in tqdm(films_to_ingest[:ingestion_limit]):
-	print('\n', film_id)
-	try:
-		ingest_film(film_id)
+# print('In total, there are {} films left to update letterboxd info for:'.format(len(films_to_ingest)))
+# for film_id in tqdm(films_to_ingest[:ingestion_limit]):
+	# print('\n', film_id)
+	# try:
+		# ingest_film(film_id)
 # 	    # update_tmbd_metadata(film_id)
 # 		# update_all_letterboxd_info(film_id)
-	except Exception as e:
-	    print('UPDATE FAILED - {}'.format(e))
+	# except Exception as e:
+	    # print('UPDATE FAILED - {}'.format(e))
 
 # update_oldest_records(film_limit=10, dryrun=False)
 # refresh_core_tables()
@@ -120,3 +120,5 @@ for film_id in tqdm(films_to_ingest[:ingestion_limit]):
 # download_letterboxd_zip(hide_actions=False)
 
 # ingest_new_films(999)
+
+run_algo()
