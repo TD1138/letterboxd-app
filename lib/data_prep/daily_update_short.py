730bf3817c44e350be1c9f4cee8edd196e256960
TD1138 <tomdevine1138@gmail.com>
2024-08-09 15:11:40 +0100

    add nozip functionality to daily update short

--- lib/data_prep/daily_update_short.py
+++ lib/data_prep/daily_update_short.py
@@ -4,15 +4,20 @@ from enrichment_utils import ingest_new_films
 from algo_utils import run_algo
 from precompute_tables import precompute_tables
 from gcp_utils import download_db, upload_db
+import sys
 
 import warnings
 warnings.filterwarnings("ignore")
 
 download_db()
-download_letterboxd_zip(hide_actions=False)
-unzip_letterboxd_downloads()
-set_latest_export()
-refresh_core_tables()
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'nozip':
+        print('Proceeding with short daily update with no download of letterboxd zip file')
+else:
+    download_letterboxd_zip(hide_actions=False)
+    unzip_letterboxd_downloads()
+    set_latest_export()
+    refresh_core_tables()
 ingest_new_films()
 run_algo('decision_tree')
 precompute_tables()
