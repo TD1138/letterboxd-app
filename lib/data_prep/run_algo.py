da70f9cb25a7db227b29616a637d420a862e279a
TD1138 <tomdevine1138@gmail.com>
2024-09-20 15:11:48 +0100

    add python script to run the algorithm

---
+++ lib/data_prep/run_algo.py
@@ -0,0 +1,11 @@
+from gcp_utils import download_db, upload_db
+from algo_utils import run_algo
+from precompute_tables import precompute_tables
+
+import warnings
+warnings.filterwarnings("ignore")
+
+download_db()
+run_algo()
+precompute_tables()
+upload_db()
