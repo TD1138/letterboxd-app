f32a11c993dffa2fa66418e4a2baffacea29ff6a
TD1138 <tomdevine1138@gmail.com>
2025-05-06 22:02:08 +0100

    misc

--- lib/data_prep/test.py
+++ lib/data_prep/test.py
@@ -5,7 +5,7 @@ from export_utils import refresh_core_tables, cleanup_exports_folder, exportfile
 from enrichment_utils import ingest_new_films, get_all_films, ingest_film, ingest_new_people
 from sqlite_utils import get_from_table, get_film_ids_from_select_statement, select_statement_to_df, replace_record
 from tmdb_utils import update_tmdb_stats, update_tmbd_metadata, update_person_metadata
-from error_utils import correct_tmdb_metadata_errors, correct_all_errors, correct_letterboxd_stats_errors
+from error_utils import correct_tmdb_metadata_errors, correct_all_errors, correct_letterboxd_stats_errors, correct_release_date_mismatches
 from update_utils import update_oldest_records, update_streaming_records, update_tmdb_metadata_records, update_recent_films, update_upcoming_films, update_letterboxd_stats, update_letterboxd_top_250
 from algo_utils import run_algo
 from selenium_utils import download_letterboxd_zip
@@ -43,14 +43,16 @@ warnings.filterwarnings("ignore")
 # precompute_table('actor_film_level_query')
 # update_portraits(500)
 
-imdb_record = {
-        'FILM_ID': 'f_02noo',
-        'IMDB_ID': 'tt0053103',
-        'CREATED_AT':datetime.now()
-    }
+# imdb_record = {
+#         'FILM_ID': 'f_02noo',
+#         'IMDB_ID': 'tt0053103',
+#         'CREATED_AT':datetime.now()
+#     }
 
-replace_record('IMDB_ID', imdb_record, film_id='f_02noo', log_reason='MANUAL_CORRECTION')
+# replace_record('IMDB_ID', imdb_record, film_id='f_02noo', log_reason='MANUAL_CORRECTION')
 
 
-upload_db()
+# upload_db()
+
+correct_release_date_mismatches()
 # update_images()
